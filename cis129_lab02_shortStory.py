fb6db0a96b8cd63acd033e754d3b2ee85279713e
mwhite41pima <mwhite41@mail.pima.edu>
2024-09-14 22:14:26 -0700

    updated the notes

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,6 +1,6 @@
 # Author: Marquies White
-#My first Python lab in my CIS129 course
-Prints a simple short story
+# My first Python lab in my CIS129 course
+# Prints a simple short story
 print('A student started their coding journey')
 print('Their journey began with simple Python code')
 print('If you're reading this, their code must have worked!')
